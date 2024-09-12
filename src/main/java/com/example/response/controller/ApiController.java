0eb4f8c06833e4c8dbd6d9f49c87156ee76c1f47
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-09-12 14:54:28 +0900

    스프링부트 5. RESPONSE 2차[완]

--- src/main/java/com/example/response/controller/ApiController.java
+++ src/main/java/com/example/response/controller/ApiController.java
@@ -1,20 +1,29 @@
 package com.example.response.controller;
 
 import com.example.response.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
+    //TEST
     @GetMapping("/text")
     public String text(@RequestParam String account){
         return account;
     }
 
     //JSON
-    @PutMapping("/json")
+    // req -> object mapper -> object -> method -> object -> object mapper -> jason -> response
+    @PostMapping("/json")
     public User json(@RequestBody User user){
         return user;
+    }
+
+    @PutMapping("/put")
+    public ResponseEntity<User> put(@RequestBody User user){
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 }
